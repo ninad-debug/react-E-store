49d080353b2db15db6dfb12f503d3f211ee9fbb3
ninad-debug <ninadsamant07@gmail.com>
2024-01-16 17:56:40 +0530

    header section layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,14 +12,38 @@ import { Context } from "../../utils/context";
 import "./Header.scss";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="main-header">
+    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
       <div className="header-content">
-        <ul>
+        <ul className="left">
           <li>Home</li>
           <li>About</li>
           <li>Categories</li>
         </ul>
+        <div className="center">REactStore.</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
       </div>
     </header>
   );
